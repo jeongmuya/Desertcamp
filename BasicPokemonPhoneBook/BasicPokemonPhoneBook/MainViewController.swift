3ca36e835445d4a31e75f597056eebd8171fb623
Yang jeong mu <105642388+jeongmuya@users.noreply.github.com>
2024-12-27 16:30:27 +0900

    Add files via upload

---
+++ BasicPokemonPhoneBook/BasicPokemonPhoneBook/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BasicPokemonPhoneBook
+//
+//  Created by YangJeongMu on 12/24/24.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+    }
+    
+}
+
+
