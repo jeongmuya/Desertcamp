402d97392516ce0382a701240e84432bc8fd13e6
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-01-05 17:44:34 +0900

    feat: 1-3 변수,상수

---
+++ 1-3variableAndConstant.playground/Contents.swift
@@ -0,0 +1,344 @@
+import UIKit
+//
+//let name: String
+//name = "Brody"
+//print(name) // name에 저장되어 있는 "Brody"가 Debug Area에 출력되는것을 확인해보세요.
+//name = "내배캠" // 오류가 발생하는것을 확인해보세요.
+//
+//var age: Int
+//age = 20
+//print(age)
+//age = 25
+//print(age)
+//
+//// 뒤로 계속 실습해보세요!
+///
+//
+
+////이름 (name)
+//let name: String
+//name = "jeongmu"
+//print(name)
+////나이 (age)
+//let age: Int
+//age = 17
+//print(age)
+////키 (height)
+//let height: Int
+//height = 190
+//print(height)
+////취미 (hobby)
+//let hobby: String
+//hobby = "클라이밍"
+//print(hobby)
+////점수 (score)
+//let score: Int
+//score = 100
+//print(score)
+////이메일 주소 (email)
+//let email: String
+//email = "asf@gmail.com"
+//print(email)
+////체중 (weight)
+//let weight: Double
+//weight = 70.7
+//print(weight)
+////전화번호 (phoneNumber)
+//let phoneNumber: String
+//phoneNumber = "010-123-123"
+//print(phoneNumber)
+////성적 (grade)
+//let grade: Double
+//grade = 99.8
+//print(grade)
+////주소 (address)
+//let address: String
+//address = "서울특별시"
+//print(address)
+////은행잔고 (balance)
+//let balance: Double
+//balance = 100.123
+//print(balance)
+////생년월일 (birthDate)
+//let birthDate: String
+//birthDate = "2010.01.11"
+//print(birthDate)
+////현재온도 (temperature)
+//let temperature: Double
+//temperature = 29.7
+//print(temperature)
+////닉네임 (nickname)
+//let nickname: String
+//nickname = "hiboy"
+//print(nickname)
+////방문자 수 (visitors)
+//let visitors: Double
+//visitors = 12459.13
+//print(visitors)
+////학교이름 (schoolName)
+//let schoolName: String
+//schoolName = "서울대학교"
+//print(schoolName)
+////이동속도 (speed)
+//let speed: Double
+//speed = 123.123
+//print(speed)
+////직업 (job)
+//let job: String
+//job = "프로그래머"
+//print(job)
+////좋아하는 숫자 (favoriteNumber)
+//let favoriteNumber: Int
+//favoriteNumber = 17
+//print(favoriteNumber)
+////국적 (nationality)
+//let nationlity: String
+//nationlity = "한국"
+//print(nationlity)
+
+
+////이름 (name)
+//var name: String
+//name = "정무"
+//print(name)
+////나이 (age)
+//var age: Int
+//age = 17
+//print(17)
+////키 (height)
+//var height: Double
+//height = 199.9
+//print(height)
+////취미 (hobby)
+//var hobby: String
+//hobby = "스케이트보드"
+//print(hobby)
+////점수 (score)
+//var score: Int
+//score = 1249
+//print(score)
+////이메일 주소 (email)
+//var email: String
+//email = "asf@gamil.com"
+//print(email)
+////체중 (weight)
+//var weight: Double
+//weight = 77.7
+//print(weight)
+////전화번호 (phoneNumber)
+//var phoneNubmer: String
+//phoneNubmer = "123-123-123"
+//print(phoneNubmer)
+////성적 (grade)
+//var grade: Double
+//grade = 99.9
+//print(grade)
+////주소 (address)
+//var address: String
+//address = "서울"
+//print(address)
+////은행잔고 (balance)
+//var balance: Double
+//balance = 4949599.123
+//print(balance)
+////생년월일 (birthDate)
+//var birthDate: String
+//birthDate = "2020.01.12"
+//print(birthDate)
+////현재온도 (temperature)
+//var temperature: Double
+//temperature = 29.7
+//print(temperature)
+////닉네임 (nickname)
+//var nickname: String
+//nickname = "브로"
+//print(nickname)
+////방문자 수 (visitors)
+//var visitors: Int
+//visitors = 100
+//print(visitors)
+////학교이름 (schoolName)
+//var schoolName: String
+//schoolName = "Hi"
+//print(schoolName)
+////이동속도 (speed)
+//var speed: Double
+//speed = 100.7
+//print(speed)
+////직업 (job)
+//var job: String
+//job = "무직백수"
+//print(job)
+////좋아하는 숫자 (favoriteNumber)
+//var favoriteNumber: String
+//favoriteNumber = "사람때리기"
+//print(favoriteNumber)
+////국적 (nationality)
+//var nationality: String
+//nationality = "미국"
+//print(nationality)
+
+////이름 (name)
+//let name: String = "jeongmu"
+////나이 (age)
+//let age: Int = 14
+////키 (height)
+//let height: Double = 199.8
+////취미 (hobby)
+//let hobby: String = "도망가기"
+////점수 (score)
+//let score: Double = 12.4
+////이메일 주소 (email)
+//let email: String = "asdf@gmail.com"
+////체중 (weight)
+//let weight: Double = 123.4
+////전화번호 (phoneNumber)
+//let phoneNumber: String = "1234-1234-1234"
+////성적 (grade)
+//let grade: Double = 984.1
+////주소 (address)
+//let address: String = "서울"
+////은행잔고 (balance)
+//let balance: Double = 12315512314.1
+////생년월일 (birthDate)
+//let birthDate: String = "2020.11.11"
+////현재온도 (temperature)
+//let temperature: Double = 32.3
+////닉네임 (nickname)
+//let nickname: String = "ff"
+////방문자 수 (visitors)
+//let visitors: Int = 123
+////학교이름 (schoolName)
+//let schoolName: String = "adsf"
+////이동속도 (speed)
+//let speed: Double = 123.4
+////직업 (job)
+//let job: String = "faf"
+////좋아하는 숫자 (favoriteNumber)
+//let favoritenumber: Int = 193
+////국적 (nationality)
+//let nationality: String = "지구"
+
+
+////이름 (name)
+//var name: String = "adsf"
+////나이 (age)
+//var age: Int = 12
+////키 (height)
+//var height: Double = 199.8
+////취미 (hobby)
+//var hobby: String = "adsf"
+////점수 (score)
+//var score: Int = 123
+////이메일 주소 (email)
+//var email: String = "asdf@gmail.com"
+////체중 (weight)
+//var weight: Double = 123.4
+////전화번호 (phoneNumber)
+//var phoneNumber: String = "010-123-123"
+////성적 (grade)
+//var grade: Double = 123.4
+////주소 (address)
+//var address: String = "asffaf"
+////은행잔고 (balance)
+//var balance: Double = 123.4
+////생년월일 (birthDate)
+//var birthDate: String = "2040.10.10"
+////현재온도 (temperature)
+//var temperature: Double = 39.4
+////닉네임 (nickname)
+//var nickname: String = "Fafasd"
+////방문자 수 (visitors)
+//var visitors: Double = 123541.1
+////학교이름 (schoolName)
+//var schoolName: String = "asf"
+////이동속도 (speed)
+//var speed: Double = 123.3
+////직업 (job)
+//var job: String = "asdf"
+////좋아하는 숫자 (favoriteNumber)
+//var favoritemNuber: Int = 14
+////국적 (nationality)
+//var nationality: String = "asdf"
+
+////이름 (name)
+//let name = "adff"
+////나이 (age)
+//let age = 123
+////키 (height)
+//let height = 199
+////취미 (hobby)
+//let hobby = "fasf"
+////점수 (score)
+//let score = 123
+////이메일 주소 (email)
+//let email = "asdf@gmail.com"
+////체중 (weight)
+//let weight = 123.3
+////전화번호 (phoneNumber)
+//let phoneNumber = "123-13-13"
+////성적 (grade)
+//let grade = 123.4
+////주소 (address)
+//let address = "adsf"
+////은행잔고 (balance)
+//let balance = 12312414.1
+////생년월일 (birthDate)
+//let birthDate = "2200.12.21"
+////현재온도 (temperature)
+//let temperature = 55.5
+////닉네임 (nickname)
+//let nickName = "asdf"
+////방문자 수 (visitors)
+//let visitors = 151512
+////학교이름 (schoolName)
+//let schoolName = "asdfas"
+////이동속도 (speed)
+//let speed = 1241515.5
+////직업 (job)
+//let jon = "adsfasdf"
+////좋아하는 숫자 (favoriteNumber)
+//let favoriteNumber = 123
+////국적 (nationality)
+//let nationality = "asdf"
+
+////이름 (name)
+//var name = "adf"
+////나이 (age)
+//var age = 123
+////키 (height)
+//var height = 124124
+////취미 (hobby)
+//var hobby = "fasdf"
+////점수 (score)
+//var score = 12314
+////이메일 주소 (email)
+//var email = "asdf@gmail.com"
+////체중 (weight)
+//var weight = 1241.5
+////전화번호 (phoneNumber)
+//var phoneNumber = "123-123-123-123"
+////성적 (grade)
+//var grade = 1234.4
+////주소 (address)
+//var address = "adsf24"
+////은행잔고 (balance)
+//var balance = 1234.1
+////생년월일 (birthDate)
+//var birthDate = "2040.11.22"
+////현재온도 (temperature)
+//var temperature = 123.2
+////닉네임 (nickname)
+//var nickName = "adsf"
+////방문자 수 (visitors)
+//var visitors = 123
+////학교이름 (schoolName)
+//var schoolName = "asdf"
+////이동속도 (speed)
+//var speed = 123.4
+////직업 (job)
+//var job = "fadsf"
+////좋아하는 숫자 (favoriteNumber)
+//var favoriteNumber = "afsd"
+////국적 (nationality)
+//var nationality = "asdf"
