db4706efb55f038f51701e240184de98c2c4d332
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-01-05 13:59:49 +0900

    feat: swift 문법종합반 1주차 1-2 Camelcase

---
+++ 1-2CamelCase.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+// CamelCase
+
+// 샘플을 보고 10개 이상으로 만들어보세요.
+
+//my name is brody // 기본 문장
+//myNameIsBrody // Camel Case
+//
+//Hello World // 기본 문장
+//HelloWorld  // Camel Case
+
+
+//1. i love programming // 기본 문장
+//   iLoveProgramming
+//   iLovePromgramming // Camel Case
+//
+//2. this is my first app// 기본 문장
+//   thisIsMyFirstApp
+//   thisIsMyFirstApp // Camel Case
+//
+//3. welcome to swift study // 기본 문장
+//   welcomeToSwiftStudy
+//   welcomeToSwiftStudy // Camel Case
+//
+//4. today is very sunny day // 기본 문장
+//   todayIsVerySunnyDay
+//   todayIsVerySunnyDay // Camel Case
+//
+//5. please save this data // 기본 문장
+//   pleaseSaveThisData
+//   pleaseSaveThisData // Camel Case
+//
+//6. open the new window // 기본 문장
+//   openTheNewWindow
+//   openTheNewWindow // Camel Case
+//
+//7. click this button to start // 기본 문장
+//   clickThisButtonToStart
+//   clickThisButtonToStart // Camel Case
+//
+//8. show user profile image // 기본 문장
+//   showUserProfileImage
+//   showUserProfileImage // Camel Case
+//
+//9. update current location now // 기본 문장
+//   updateCurrentLocationNow
+//   updateCurrentLocationNow // Camel Case
+//
+//10. send message to friend // 기본 문장
+//    sendMessageToFriend
+//    sendMessageToFriend // Camel Case
+//
+//11. create new account here // 기본 문장
+//    createNewAccountHere
+//    createNewAccountHere // Camel Case
+//
+//12. download file from server // 기본 문장
+//    downloadFileFromServer
+//    downloadFileFromServer // Camel Case
+//
+
+
+//my car is fast
+//myCarIsFast // 이렇게 수정해주세요.
+//she loves coding
+//sheLovesCoding
+//we are learning swift
+//WeAreLearningSwift
+//the sky is blue
+//theSkyIsBlue
+//i like coffee
+//iLikeCoffee
+//my favorite color is red
+//myFavoriteColorIsRed
+//this is a test
+//thisIsATest
+//programming is fun
+//programmingIsFun
+//apple makes great products
+//appleMakesGreatProducts
+//teamwork is important
+//teamworkIsImportant
+//the cat is sleeping
+//theCatIsSleeping
+//i want to travel
+//iWantToTravel
+//we are developers
+//weAreDevelopers
+//learning swift is easy
+//learningSwiftIsEasy
+//my computer is new
+//myComputerIsNew
